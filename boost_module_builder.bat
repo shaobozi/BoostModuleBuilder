5650c0e0e460f55be81090ea2229006c4fc346c6
shaobo.zi <zishaobo@rochern.com>
2015-12-10 11:06:12 +0800

    Detailized description.

--- boost_module_builder.bat
+++ boost_module_builder.bat
@@ -14,6 +14,7 @@ IF defined param_missed (
 	ECHO module_name: the module name going to be built
 	ECHO boost_src_root: Specify the root directory of boost ^(e.g. D:\boost_1_54_0\^)
 	ECHO build_dir[optional]: Where to build the filesystem module
+	ECHO NOTE: DO NOT FORGET THE BACK SLASH AT THE END OF PATH
 	ECHO.
 	ECHO e.g. boost_module_build.bat filesystem D:\boost_1_54_0\ E:\boost_file_system
 	ECHO.
@@ -31,7 +32,20 @@ IF [%3]==[] (
 
 :: build filesystem module
 :: b2.exe --help to get a reference of all options and properties
-"%~dp2b2.exe" --build-type=complete --build-dir=%dest% --with-%1 toolset=msvc-10.0 variant=release,debug link=shared threading=multi runtime-link=shared
+set buildArgs=--build-type=complete --build-dir=%dest% --with-%1 toolset=msvc-10.0 variant=release,debug link=shared threading=multi runtime-link=shared
+
+:: print final command
+echo ------- The Command running------
+echo %~dp2b2.exe %buildArgs%
+echo 
+
+:: Run 
+"%~dp2b2.exe" %buildArgs%
+
+:: print final command
+echo ------- The Command runded------
+echo %~dp2b2.exe %buildArgs%
+
 popd
 
 :EOF
